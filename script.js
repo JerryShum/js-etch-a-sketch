042e1ee6c17497ada32f03d7738a1df11a52c975
JerryShum <jerryshum2636@gmail.com>
2024-01-05 02:54:25 -0500

    feat: "grid" of divs is semi working
    still need to make it a 16x16 grid

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ const gridContainerElement = document.querySelector('.grid-container');
 //! Logic
 
 // Inserting n elements into gridContainerElement
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 16 * 16; i++) {
     gridContainerElement.insertAdjacentHTML('beforeend', "<div class='grid-node'></div>")
 }
