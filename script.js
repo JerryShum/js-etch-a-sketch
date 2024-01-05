23607503e1f042763f08d014afc4bb3ba3452626
JerryShum <jerryshum2636@gmail.com>
2024-01-05 02:45:12 -0500

    feat: inserting div elements into container works

---
+++ script.js
@@ -0,0 +1,10 @@
+//! Selecting/Creating Elements
+const gridContainerElement = document.querySelector('.grid-container');
+
+
+//! Logic
+
+// Inserting n elements into gridContainerElement
+for (let i = 0; i < 16; i++) {
+    gridContainerElement.insertAdjacentHTML('beforeend', "<div class='grid-node'></div>")
+}
