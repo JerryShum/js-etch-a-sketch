2c3e6b95f2fe199771e366c046694c177d82925b
JerryShum <jerryshum2636@gmail.com>
2024-01-06 01:43:42 -0500

    feat: 16x16 grid is working (nested loop)

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 //! Selecting/Creating Elements
 const gridContainerElement = document.querySelector('.grid-container');
+const gridRowElement = document.createElement('div');
+
+console.log(gridRowElement);
 
 
 //! Logic
 
+let n = 16;
+
 // Inserting n elements into gridContainerElement
-for (let i = 0; i < 16 * 16; i++) {
-    gridContainerElement.insertAdjacentHTML('beforeend', "<div class='grid-node'></div>")
+for (let i = 0; i < n; i++) {
+    let clone = gridRowElement.cloneNode(true);
+    clone.className = "grid-row"
+    gridContainerElement.appendChild(clone)
+
+    for (let j = 0; j < n; j++) {
+        clone.insertAdjacentHTML('beforeend', "<div class='grid-node'></div>")
+    }
+
+
 }
